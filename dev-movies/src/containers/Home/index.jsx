4a12475d2685af1b247849628d3fb2ac50a4697b
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-04-13 19:22:46 -0300

    utilizando o useEffect para deixar mais dinâmico.

--- dev-movies/src/containers/Home/index.jsx
+++ dev-movies/src/containers/Home/index.jsx
@@ -1,18 +1,33 @@
 import api from '../../services/api'
+import { Background } from './styles'
+import { useState, useEffect } from 'react'
 
 function Home() {
-  async function getMovies() {
-    const data = await api.get('/movie/popular')
+  const [movie, setMovie] = useState([])
 
-    console.log(data)
-  }
+  useEffect(() => {
+    async function getMovies() {
+      const {
+        data: { results }
+      } = await api.get('/movie/popular')
 
-  getMovies()
+      setMovie(results[1])
+    }
+
+    getMovies()
+  }, [])
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      {movie && (
+        <Background
+          img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        >
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+        </Background>
+      )}
+    </>
   )
 }
 
